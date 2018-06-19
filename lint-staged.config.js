80af9962694ce830fc41f9b8b37b75f4f95ba57c
Diego Stratta <strattadb@gmail.com>
2018-06-19 01:11:35 -0300

    fix: fix git pre commit hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.json': ['prettier --write', 'git add'],
-  '*.{js,jsx}': ['prettier-eslint --write', 'git add'],
+  '*.{js,jsx}': ['prettier --write', 'git add'],
   '*.{ts,tsx}': ['prettier --write', 'tslint --fix', 'git add'],
 };
