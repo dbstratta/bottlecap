81b2983619039a3277b5081c199cbbf7b7ee57b2
Diego Stratta <strattadb@gmail.com>
2018-06-25 23:37:41 -0300

    chore: add lint tasks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,6 @@ module.exports = {
   '*.json': ['prettier --write', 'git add'],
   '*.{js,jsx}': ['prettier-eslint --write', 'git add'],
   '*.{ts,tsx}': ['prettier --write', 'tslint --fix', 'git add'],
+  '*.md': ['markdownlint', 'git add'],
+  '*.sh': ['shellcheck', 'git add'],
 };
